ede696a7e9c2de0804f4a1ce8e7bc315f17a00db
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-01 19:08:34 +0530

    Added variables program demonstrating declaration and value update

---
+++ Fundamentals/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name = "Janhavi";
+    int age = 18;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    int updatedAge = 19;
+    cout << "Updated Age: " << updatedAge << endl;    return 0;
+}
