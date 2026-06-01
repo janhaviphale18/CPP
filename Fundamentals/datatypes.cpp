1ac0cc4d56144ef33dfa7565d1caf51c3231b69f
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-01 21:36:04 +0530

    Added datatypes program demonstrating int float double char and bool

---
+++ Fundamentals/datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 18;
+    float percentage = 99.9;
+    double height = 5.3;
+    char grade = 'A';
+    bool isStudent = true;
+    cout << "Age: " << age << endl;
+    cout << "Percentage: " << percentage << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+    return 0;
+}
