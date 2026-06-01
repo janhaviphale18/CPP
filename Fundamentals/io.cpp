5f8c0a2f05e4e52cd535b9742ab2e0e3e1236521
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-01 23:48:48 +0530

    Added input output program demonstrating cin and cout

---
+++ Fundamentals/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    string name;
+    cout << "Enter Your Name: ";
+    cin >> name;
+    int age;
+    cout << "Enter Your Age: ";
+    cin >> age;
+    cout << "Your Name is: " << name << endl;
+    cout << "Your Age is: " << age << endl;
+    return 0;
+}
