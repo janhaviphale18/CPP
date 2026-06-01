9190df980c839650edebd6de2601391c9fb6b4cd
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-01 23:24:56 +0530

    Added constants program explaining fixed values using const

---
+++ Fundamentals/constants.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const float pi = 3.14;
+    cout << "The value of pi is: " << pi << endl;
+    const double g = 9.8;
+    cout << "The value of g is: " << g << endl;
+    const int days_of_weeks = 7;
+    cout << "The number of days in a week are: " << days_of_weeks << endl;
+    return 0;
+}
