b43417cc63d17830441891bb73fd0f98ff9d2df4
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-01 18:03:51 +0530

    Added comments program explaining single and multiline comments

---
+++ Fundamentals/comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // This is a single line comment
+    cout << "This is a single line comment" << endl;
+    cout << "Single line comments are used for short explanations" << endl;
+    /* This is a multiline comment
+    Multiline comments are used for longer explanations
+    Multiline comments are used to comment out blocks of code */
+    cout << "This is a multiline comment" << endl;
+    cout << "Multiline comments can span multiple lines" << endl;
+    return 0;
+}
