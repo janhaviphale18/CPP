d39e9927e2fbb74625605c107f761322053f66e9
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-02 22:38:40 +0530

    Added escape sequences program demonstrating formatted output

---
+++ Fundamentals/escape.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello\nWorld" << endl;
+    cout << endl;
+    cout << "Name\tAge" << endl;
+    cout << "Janhavi\t18" << endl;
+    cout << endl;
+    cout << "He said \"Hello\"" << endl;
+    cout << endl;
+    cout << "Folder Path: C:\\CPP\\Fundamentals" << endl;
+
+    return 0;
+}
