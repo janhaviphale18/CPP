9d895885b2af67117daf7de8fbd84f11a23b0480
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-02 22:36:01 +0530

    Added ASCII program demonstrating character and integer conversion

---
+++ Fundamentals/ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char letter = 'A';
+    cout << "Character: " << letter << endl;
+    cout << "ASCII Value of A: " << (int)letter << endl;
+    cout << endl;
+
+    int asciiValue = 66;
+
+    cout << "ASCII Number: " << asciiValue << endl;
+    cout << "Character of ASCII 66: " << (char)asciiValue << endl;
+
+    return 0;
+}
