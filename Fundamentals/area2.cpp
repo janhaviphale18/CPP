8b897cfb4467b671a85e3f74335dbb6f61f94739
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-02 22:49:05 +0530

    Added square area program using user input and formula

---
+++ Fundamentals/area2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int side;
+    int area;
+
+    cout << "Enter Side of Square: ";
+    cin >> side;
+    area = side * side;
+    cout << endl;
+    cout << "Area of Square: " << area << endl;
+
+    return 0;
+}
