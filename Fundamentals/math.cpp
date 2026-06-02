a9928266d52489b94abd49461aadf242ebabbb94
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-02 22:41:43 +0530

    Added math functions program demonstrating sqrt pow and abs

---
+++ Fundamentals/math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int number = 25;
+
+    cout << "Square Root of 25:" << sqrt(number) << endl;
+    cout << "2 raised to power 3: " << pow(2, 3) << endl;
+    cout << "Absolute Value of -10: " << abs(-10)<< endl;
+
+    return 0;
+}
