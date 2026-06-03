e66c7d5fa7becc24aab06d4bc488a2123def5d4c
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-03 13:30:26 +0530

    Added multiple input program demonstrating user entered values

---
+++ Fundamentals/multi_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int firstNumber;
+    int secondNumber;
+    int sum;
+    
+    cout << "Enter the Two Numbers: ";
+    cin >> firstNumber >> secondNumber;
+    sum = firstNumber + secondNumber;
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
