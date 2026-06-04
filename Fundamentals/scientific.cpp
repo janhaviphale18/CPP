b8fc7a63d9b440be69e2ce5538c98f24f287296c
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-04 19:56:10 +0530

    Added scientific notation program demonstrating large and small numbers

---
+++ Fundamentals/scientific.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    double largeNumber = 1e10;
+    double smallNumber = 1e-10;
+    cout << "Large Number: " << largeNumber << endl;
+    cout << "Small Number: " << smallNumber << endl;    
+    return 0;
+}
