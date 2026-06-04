dcc72f0c6e7ba1e784979084c59b44933e1ee907
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-04 13:48:39 +0530

    Added operator precedence program demonstrating expression evaluation

---
+++ Fundamentals/precedence.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int result1 = 10 + 5 * 2;
+    int result2 = (10 + 5) * 2;
+    cout << "Result without brackets: " << result1 << endl; 
+    cout << "Result with brackets: " << result2 << endl;
+    return 0;
+}
