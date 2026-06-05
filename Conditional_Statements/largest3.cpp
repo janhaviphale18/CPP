52c9baa7e9dd9a545fcd00edce6d55bd85fdda94
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-05 20:22:42 +0530

    Added largest of three numbers program

---
+++ Conditional_Statements/largest3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter third number: ";
+    cin >> num3;
+
+    if (num1 >= num2 && num1 >= num3)
+    {
+        cout << "Largest number is: " << num1;
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        cout << "Largest number is: " << num2;
+    }
+    else
+    {
+        cout << "Largest number is: " << num3;
+    }
+
+    return 0;
+}
