85a7cda6ee54f41d0ca62bb6c5fddd02e5301bff
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-11 21:50:25 +0530

    Added continuous number triangle pattern program

---
+++ Loops/pattern6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    int num = 1;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << num << " ";
+            num++;
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
