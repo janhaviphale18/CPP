2d6eb7b60200c65df2ccd00129105ddcd48686a8
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-11 19:34:17 +0530

    Added right angled triangle pattern program

---
+++ Loops/pattern1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int row = 1; row <= rows; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
