e296b2766728ee81841424797115cd5958893c1a
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-14 17:39:47 +0530

    Added no argument no return function program

---
+++ Functions/greet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void greet()
+{
+    cout << "Greetings from Janhavi!!!" << endl;
+    cout << "Welcome to Functions in C++" << endl;
+}
+int main()
+{
+    greet();
+    return 0;
+}
