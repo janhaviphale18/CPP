d696ab73837f38c31f4f3871615629dd03bcd920
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-15 08:52:03 +0530

    Added inline function program

---
+++ Functions/inline.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+inline int square (int num)
+{
+    return num * num;
+}
+int main()
+{
+    int number;
+    cout <<"Enter a number: ";
+    cin >> number;
+    cout << "Square of " << number << " is " << square(number) << endl;
+    return 0;
+}
