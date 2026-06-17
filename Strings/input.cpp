c9232f59692af7f0cb56ab6c257f1d9f4c054ec6
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-17 14:49:02 +0530

    Added string input with spaces using getline

---
+++ Strings/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string name;
+    cout << "Enter your name: ";
+    getline(cin , name);
+    cout << "Your Name is: " << name << endl;
+    return 0;
+}
