0a662c24ba81c0ee688dcb3a61594953e495fb47
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-21 14:24:52 +0530

    Added dynamic memory allocation program

---
+++ Pointers/dynamic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *ptr = new int;
+    *ptr = 50;
+
+    cout << "Value = " << *ptr << endl;
+    cout << "Address = " << ptr << endl;
+
+    return 0;
+}
