b8355746649b1435f633ad08c2f0250dfcbc162c
Janhavi Phale <janhaviphale0@gmail.com>
2026-06-22 19:24:10 +0530

    Added file reading program

---
+++ File_Handling/read.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream file("sample.txt");
+    string line;
+
+    while (getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+
+    return 0;
+}
